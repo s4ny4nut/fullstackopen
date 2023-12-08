5000e748beea6a60fecf401cda5db88bbce22637
Alexander Sorochinskiy <alexander.sorochinskiy@gmail.com>
2023-12-08 03:50:02 +0200

    part3 fixes

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-
 const app = express();
 
 const requestLogger = (request, response, next) => {
@@ -25,29 +24,21 @@ app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {    
+    return response.status(400).json({ error: error.message })  
+  } else {
+    return response.status(500).json({ error: 'internal server error' })
+  }
+}
 
 
 app.get("/info", (request, response, next) => {
@@ -131,23 +122,6 @@ app.put("/api/persons/:id", (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {    
-    return response.status(400).json({ error: error.message })  
-  } else {
-    return res.status(500).json({ error: 'internal server error' })
-  }
-}
-
-
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
